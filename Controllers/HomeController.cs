156fd45e8ad80e13fb18e426c13647fd95f48b82
Robson Rodrigues da Silva <robson_rodriguessilva@outlook.com>
2022-03-09 10:18:22 -0300

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using EspacoDisco.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace EspacoDisco.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            List<DiskInformation> listaInformações = new List<DiskInformation>();;
+            foreach (DriveInfo drive in drives)
+            {
+                listaInformações.Add(new DiskInformation() { 
+                    DiskFreeSpace = String.Format("{0:0.##}GB", drive.TotalFreeSpace / 1024d / 1024d / 1024d),
+                    DiskName = drive.Name,
+                    DiskTotalSpace = String.Format("{0:0.##}GB", drive.TotalSize / 1024d / 1024d / 1024d)
+                });
+            }
+
+            return View(new DiskInformationViewModel { listaInformações = listaInformações});
+        }
+    }
+}
---
+++ Models/DiskInformationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace EspacoDisco.Models
+{
+    public class DiskInformationViewModel
+    {
+        public List<DiskInformation> listaInformações;
+    }
+
+    public class DiskInformation 
+    {
+        public string DiskName { get; set; }
+        public string DiskFreeSpace { get; set; }
+        public string DiskTotalSpace { get; set; }
+    }
+
+}
